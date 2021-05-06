037d76d05334f25fd9ea4c93f7475fb10f250540
Ahmad Saufi Maulana <ahmadsaufimaulana@gmail.com>
2021-05-07 05:15:17 +0800

    fixed directory problem - post test

---
+++ Session30_Cloud_Intro/src/main/java/dev/azemoning/starter/controller/ProductController.java
@@ -0,0 +1,41 @@
+package dev.azemoning.starter.controller;
+
+import dev.azemoning.starter.model.Product;
+import dev.azemoning.starter.service.ProductServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1")
+public class ProductController {
+
+    @Autowired
+    private ProductServiceImpl productService;
+
+    @GetMapping("product")
+    public ResponseEntity<Object> getProducts() {
+        return new ResponseEntity<>(productService.getProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("product/{id}")
+    public ResponseEntity<Object> getProductById(@PathVariable Long id) {
+        return new ResponseEntity<>(productService.getProductById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("product")
+    public ResponseEntity<Object> addProduct(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.addProduct(product), HttpStatus.CREATED);
+    }
+
+    @PutMapping("product/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return new ResponseEntity<>(productService.updateProduct(id, product), HttpStatus.OK);
+    }
+
+    @DeleteMapping("product/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable Long id) {
+        return new ResponseEntity<>(productService.deleteProduct(id), HttpStatus.OK);
+    }
+}
